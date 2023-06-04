439d240c1e727d2cce9a0f9a6cda37ac15cd69e5
JarekWnuk <j.p.wnuk@gmail.com>
2023-06-04 20:36:29 +0100

    Moves the choice list inside the get_computer_choice function.

--- manual_rps.py
+++ manual_rps.py
@@ -1,16 +1,15 @@
 import random
 
-choice_list =["Rock", "Paper", "Scissors"]
-
 # gets random choice for computer
 def get_computer_choice():
+    choice_list =["Rock", "Paper", "Scissors"]
     return random.choice(choice_list)
 
 # asks user for choice
 def get_user_choice():
     while True:
         choice = input("Choose from: rock, paper, scissors:\n")
-        if choice.capitalize() in choice_list:
+        if choice.capitalize() in ["Rock", "Paper", "Scissors"]:
             break
         else:
             print("Incorrect input, try again.")
