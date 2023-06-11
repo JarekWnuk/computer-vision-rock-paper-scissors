e2fa322b74441cbf9ecc19abd50c7dfbe69f0bea
JarekWnuk <j.p.wnuk@gmail.com>
2023-06-11 16:08:14 +0100

    Adds countdown function.

--- camera_rps.py
+++ camera_rps.py
@@ -1,10 +1,34 @@
 import cv2
+import time
 from keras.models import load_model
 import numpy as np
+
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 class_names = open("labels.txt", "r").readlines()
+
+def countdown():
+    start = time.time()
+    print(5)
+    time_to_print = [4, 3, 2, 1]
+    while True:
+        total = time.time() - start
+        if total > 1 and total < 2 and 4 in time_to_print:
+            print(4)
+            time_to_print.pop(0)
+        elif total > 2 and total < 3 and 3 in time_to_print:
+            print(3)
+            time_to_print.pop(0)
+        elif total > 3 and total < 4 and 2 in time_to_print:
+            print(2)
+            time_to_print.pop(0)
+        elif total > 4 and total < 5 and 1 in time_to_print:
+            print(1)
+            time_to_print.pop(0)
+        elif total > 5:
+            print("Show your hand!")
+            break  
 
 def get_prediction():
     while True: 
@@ -21,5 +45,6 @@ def get_prediction():
             class_name = class_names[index]
             break
     return class_name
-    
-get_prediction()
+
+countdown_five()
+#get_prediction()
