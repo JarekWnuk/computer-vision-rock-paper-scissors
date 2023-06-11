d82ef99a35c259c2df7c7a55e94a0ff6061927ff
JarekWnuk <j.p.wnuk@gmail.com>
2023-06-11 09:50:27 +0100

    Corrects error in function name. Function returns predicted class name.

--- camera_rps.py
+++ camera_rps.py
@@ -6,7 +6,7 @@ cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 class_names = open("labels.txt", "r").readlines()
 
-def get_prdiction():
+def get_prediction():
     while True: 
         ret, frame = cap.read()
         resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
@@ -19,9 +19,7 @@ def get_prdiction():
         # Press q to close the window
         if cv2.waitKey(1) & 0xFF == ord('q') or np.argmax(prediction) !=3 and prediction[0][index] > 0.8 :
             class_name = class_names[index]
-            confidence_score = prediction[0][index]
-            print(class_name)
-            print(confidence_score)
             break
-
-get_prdiction()
+    return class_name
+    
+get_prediction()
